981948b5cfc8347d646884d7d751d7b41060e4a1
A.EibarSegales <andysejas444@gmail.com>
2024-04-14 23:54:11 -0400

    avanceBasic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import Card from './assets/componentes/Card';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hola React</h1>
-      <div className='container'>
         <Card />
-      </div>
-    </div>
   );
 }
 
--- src/assets/componentes/Card.jsx
+++ src/assets/componentes/Card.jsx
@@ -1,12 +1,29 @@
 
+import { useState } from 'react'
+import React from 'react'
+import './Card.css'
 
 function Card() {
+
+    const [name,setName] = useState('Andrew Eibar Segales Sejas');
+    const [job, setJob] = useState('Developer');
+    const [about, setAbout] = useState('Soy un desarrollador web, estoy estudiando ingenieria de sistemas');
+
     return (
-        <div className="Card">
-            <h2>Titulo tarjeta</h2>
-            <p>Descripsion de la tarjeta</p>
+        <div className='Card'>
+            <div className='uppeContainer'>
+                <div className='image'>
+                    <img src="/src/assets/images/me.jpg" alt="andrew.img" height="100px" width="100px" />
+                </div>
+            </div>
+            <div className='lowerContainer'>
+                <h3>{name}</h3>
+                <h4>{job}</h4>
+                <p>{about}</p>
+                <button>Visit Profile</button>
+            </div>
         </div>
-    );
+    )
 }
 
-export default Card;
+export default Card

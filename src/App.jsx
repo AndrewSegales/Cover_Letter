51b17f94e04adb778962de247554c0a29330e925
A.EibarSegales <andysejas444@gmail.com>
2024-04-14 21:30:23 -0400

    primer_paso

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './assets/componentes/Card';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hola React</h1>
+      <div className='container'>
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/assets/componentes/Card.jsx
@@ -0,0 +1,12 @@
+
+
+function Card() {
+    return (
+        <div className="Card">
+            <h2>Titulo tarjeta</h2>
+            <p>Descripsion de la tarjeta</p>
+        </div>
+    );
+}
+
+export default Card;
